6a6fa5a27b1fd248dbde575b7238947a031d0668
n-kats <n-kats19890214@hotmail.co.jp>
2024-05-25 20:10:16 +0900

    fix image_storage

--- typed_storage/presets/image_storage.py
+++ typed_storage/presets/image_storage.py
@@ -1,29 +1,44 @@
-from typing import Callable
+from typing import Callable, TypeVar, Type
 from pathlib import Path
 
 from PIL import Image
 from typed_storage.core import TypedStorage
 
 
+class ImageBase:
+    """Base class for image storage"""
+
+    def __init__(self, image: Image.Image):
+        self.__image = image
+
+    @property
+    def image(self):
+        return self.__image
+
+
+_T_IMAGE = TypeVar("_T_IMAGE", bound=ImageBase)
+
+
 def image_storage(
+    image_type: Type[_T_IMAGE],
     directry: Path,
-    to_sub_path: Callable[[str], str],
+    to_sub_path: Callable[[_T_IMAGE], str],
 ):
-    def to_path(item: str):
+    def to_path(item: _T_IMAGE) -> str:
         return str(directry / to_sub_path(item))
 
-    def save(item: Image.Image, path: str):
+    def save(item: _T_IMAGE, path: str) -> None:
         Path(path).parent.mkdir(parents=True, exist_ok=True)
-        item.save(path)
+        item.image.save(path)
 
-    def load(path: str) -> Image.Image:
-        return Image.open(path)
+    def load(path: str) -> _T_IMAGE:
+        return image_type(Image.open(path))
 
-    def is_loadable_fn(path: str):
+    def is_loadable_fn(path: str) -> bool:
         return Path(path).is_relative_to(directry)
 
     return TypedStorage(
-        Image.Image,
+        image_type,
         to_path=to_path,
         save_fn=save,
         load_fn=load,
